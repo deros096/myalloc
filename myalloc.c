7865501efb8152e4a8c6920edf484a070c8afd0b
Caleb Derosier <deros096@d.umn.edu>
2019-10-12 10:44:28 -0500

    commit for deros096

--- myalloc.c
+++ myalloc.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "myalloc.h"
+#include <pthread.h>
 
 /* change me to 1 for more debugging information
  * change me to 0 for time testing and to clear your mind
@@ -10,24 +11,27 @@
 void *__heap = NULL;
 node_t *__head = NULL;
 
+
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
 header_t *get_header(void *ptr) {
   return (header_t *) (ptr - sizeof(header_t));
 }
 
 void print_header(header_t *header) {
   printf("[header_t @ %p | buffer @ %p size: %lu magic: %08lx]\n",
-         header,
-         ((void *) header + sizeof(header_t)),
-         header->size,
-         header->magic);
+	 header,
+	 ((void *) header + sizeof(header_t)),
+	 header->size,
+	 header->magic);
 }
 
 void print_node(node_t *node) {
   printf("[node @ %p | free region @ %p size: %lu next: %p]\n",
-         node,
-         ((void *) node + sizeof(node_t)),
-         node->size,
-         node->next);
+	 node,
+	 ((void *) node + sizeof(node_t)),
+	 node->size,
+	 node->next);
 }
 
 void print_freelist_from(node_t *node) {
@@ -38,13 +42,65 @@ void print_freelist_from(node_t *node) {
   }
 }
 
+void swapNodes(node_t** head_ref, node_t* currHead,
+	       node_t* minAddr, node_t* beforeMinAddr)
+{
+  // Make minAddr the head
+  *head_ref = minAddr;
+
+  // Change prev next to be the current head before the swap
+  beforeMinAddr->next = currHead;
+
+  // Swap the current head with the new head (minAddr)
+  node_t* temp = minAddr->next;
+  minAddr->next = currHead->next;
+  currHead->next = temp;
+}
+
+node_t* sort_free_list(node_t* head)
+{
+  if (head == NULL) // list is empty
+    return head;
+
+  if (head->next == NULL) // there is only a single node
+    return head;
+
+  // smallest address
+  node_t* minAddr = head;
+
+  // address before the smallest one
+  node_t* beforeMinAddr = NULL;
+  node_t* ptr;
+
+  // traverse the list till the last node
+  for (ptr = head; ptr->next != NULL; ptr = ptr->next)
+  {
+    if ((void*) ptr->next < (void*)minAddr)
+  	{
+  	  minAddr = ptr->next;
+  	  beforeMinAddr = ptr;
+  	}
+  }
+
+  // smallest address isn't at the start of the list
+  if (minAddr != head) swapNodes(&head, head, minAddr, beforeMinAddr);
+
+  head->next = sort_free_list(head->next);
+
+  return head;
+}
+
+
 void coalesce_freelist() {
   /* coalesce all neighboring free regions in the free list */
 
   if (DEBUG) printf("In coalesce freelist...\n");
+
+  pthread_mutex_lock(&lock);
+  __head = sort_free_list(__head); // sort the free list
   node_t *target = __head;
   node_t *node = target->next;
-  node_t *prev = target;
+  pthread_mutex_unlock(&lock);
 
   /* traverse the free list, coalescing neighboring regions!
    * some hints:
@@ -54,6 +110,19 @@ void coalesce_freelist() {
    * --> see print_free_list_from for basic code for traversing a
    *     linked list!
    */
+  while(target != NULL)
+  {
+    while(node != NULL)
+  	{
+  	  if((node == ((void*)target + sizeof(header_t) + target->size)))
+	    {
+	      target->size = target->size + node->size + sizeof(header_t);
+	      target->next = node->next;
+	    }
+  	  node = node->next;
+  	}
+    target = target->next;
+  }
 }
 
 void destroy_heap() {
@@ -77,9 +146,12 @@ void init_heap() {
     exit(1);
   }
 
+
+  pthread_mutex_lock(&lock);
   __head = (node_t *) __heap;
   __head->size = HEAPSIZE - sizeof(header_t);
   __head->next = NULL;
+  pthread_mutex_unlock(&lock);
 
   if (DEBUG) printf("heap: %p\n", __heap);
   if (DEBUG) print_node(__head);
@@ -87,15 +159,94 @@ void init_heap() {
 }
 
 void *first_fit(size_t req_size) {
+
   void *ptr = NULL; /* pointer to the match that we'll return */
 
-  if (DEBUG)
-    printf("In first_fit with size: %u and freelist @ %p\n",
-           (unsigned) req_size, __head);
+  if (DEBUG) printf("In first_fit with size: %u and freelist @ %p\n", (unsigned) req_size, __head);
 
   node_t *listitem = __head; /* cursor into our linked list */
   node_t *prev = NULL; /* if listitem is __head, then prev must be null */
   header_t *alloc; /* a pointer to a header you can use for your allocation */
+
+  while(listitem != NULL)
+  {
+    if (DEBUG) printf("[DEBUG] In loop!\n");
+    long unsigned origSize = listitem->size;
+
+    if((listitem->size >= (req_size) && req_size > 0))
+  	{
+  	  if (DEBUG) printf("listitem->size: %lu\n", listitem->size);
+  	  if (DEBUG) printf("req_size: %lu\n", req_size);
+  	  if (DEBUG) printf("origSize: %lu\n", origSize);
+
+  	  long unsigned diff = origSize - (req_size) - sizeof(header_t);
+
+  	  node_t *new_node = NULL;
+  	  node_t *next = listitem->next;
+
+
+  	  // there is only one node on the freelist and the node size is not enough space for the allocation with the header
+  	  if(prev == NULL && next == NULL && (listitem->size < (req_size + sizeof(header_t))))
+	    {
+	      return ptr;
+	    }
+
+  	  alloc = (void*) listitem;
+  	  alloc->magic = HEAPMAGIC;
+  	  alloc->size = req_size;
+
+  	  ptr = (void*) alloc + sizeof(header_t);
+
+  	  if((origSize - req_size) >= (sizeof(header_t)))
+	    {
+	      new_node = (void*) ptr + req_size;
+	      new_node->size = diff;
+	      new_node->next = next;
+	    }
+  	  else
+      {
+  	    // there isn't enough room for the header, so we alloc the entire node to req_size
+  	    alloc->size = origSize;
+  	  }
+
+
+  	  if(prev == NULL)  // edge case (listitem is head)
+	    {
+	      if (DEBUG) printf("[DEBUG] Found 'listitem is head/first free object' edge case!\n");
+
+	      if(new_node != NULL) __head = new_node;
+	      else __head = next;
+
+	      break;
+
+	    }
+  	  else //else if(next == NULL) // Edge case (listitem is last free object)
+	    {
+	      if (DEBUG) printf("[DEBUG] Found 'listitem is last free object' edge case!\n");
+	      if(new_node != NULL)
+    		{
+    		  prev->next = new_node; //+ (req_size + sizeof(header_t));
+    		  alloc->size = origSize - req_size;
+    		  //new_node->next = NULL;
+    		}
+	      else
+    		{
+    		  prev->next = next;
+    		}
+	      break;
+
+	    }
+
+  	  // if (DEBUG) printf("[DEBUG] Found 'end of else if' edge case!\n");
+  	  // prev->next = new_node + (req_size + sizeof(header_t));
+  	  // new_node->size = diff;
+  	  break;
+
+  	}
+    prev = listitem;
+    prev->size = listitem->size;
+    listitem = listitem->next;
+  }
 
   /* traverse the free list from __head! when you encounter a region that
    * is large enough to hold the buffer and required header, use it!
@@ -122,7 +273,6 @@ void *first_fit(size_t req_size) {
   return ptr;
 
 }
-
 /* myalloc returns a void pointer to size bytes or NULL if it can't.
  * myalloc will check the free regions in the free list, which is pointed to by
  * the pointer __head.
@@ -142,7 +292,9 @@ void *myalloc(size_t size) {
   /* search __head for first fit */
   if (DEBUG) printf("Going to do allocation.\n");
 
+  pthread_mutex_lock(&lock);
   ptr = first_fit(size); /* all the work really happens in first_fit */
+  pthread_mutex_unlock(&lock);
 
   if (DEBUG) printf("__head is now @ %p\n", __head);
 
@@ -157,7 +309,6 @@ void *myalloc(size_t size) {
  * undefined.
  */
 void myfree(void *ptr) {
-
   if (DEBUG) printf("\nIn myfree with pointer %p\n", ptr);
 
   header_t *header = get_header(ptr); /* get the start of a header from a pointer */
@@ -171,24 +322,48 @@ void myfree(void *ptr) {
     printf("The heap is corrupt!\n");
     return;
   }
+  else
+  {
 
-  /* free the buffer pointed to by ptr!
-   * To do this, save the location of the old head (hint, it's __head).
-   * Then, change the allocation header_t to a node_t. Point __head
-   * at the new node_t and update the new head's next to point to the
-   * old head. Voila! You've just turned an allocated buffer into a
-   * free region!
-   */
+    pthread_mutex_lock(&lock);
+    int alreadyFree = 0;
+    node_t *checkFree = __head;
 
-  /* save the current __head of the freelist */
-  /* ??? */
+    // Run through the free list to see if ptr has already been freed
+    while(checkFree != NULL) //CHANGE
+    {
+    	if(checkFree == ptr) alreadyFree = 1;
+    	checkFree = checkFree->next;
+    }
 
-  /* now set the __head to point to the header_t for the buffer being freed */
-  /* ??? */
+    // Only free ptr if it hasn't already been freed
+    if(alreadyFree == 0)
+    {
+    	//print_freelist_from(__head);
+    	/* free the buffer pointed to by ptr!
+    	 * To do this, save the location of the old head (hint, it's __head).
+    	 * Then, change the allocation header_t to a node_t. Point __head
+    	 * at the new node_t and update the new head's next to point to the
+    	 * old head. Voila! You've just turned an allocated buffer into a
+    	 * free region!
+    	 */
 
-  /* set the new head's next to point to the old head that you saved */
-  /* ??? */
+    	node_t *tempPtr = ptr;
+    	tempPtr->size = header->size;
 
-  /* PROFIT!!! */
+    	/* save the current __head of the freelist */
+    	node_t *tempHead = __head;
+    	tempHead->size = __head->size;
+
+    	/* now set the __head to point to the header_t for the buffer being freed */
+    	__head = (void*)header;
+
+    	/* set the new head's next to point to the old head that you saved */
+    	__head->next = tempHead;
+
+    	pthread_mutex_unlock(&lock);
+    }
+  }
 
 }
+
